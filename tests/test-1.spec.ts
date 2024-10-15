818b1347329fc538dcabf91f508b7af872d16243
uditkandari <udit.kandari@bbc.com>
2024-10-15 22:01:44 +0530

    deleted some tests

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://bbcw-rm-sandbox512.uat.mymediabox.com/login-form.html?_rtnlink=/rm');
-  await page.getByLabel('Email').click();
-  await page.getByLabel('Email').fill('udit.kandari@bbc.com');
-  await page.getByLabel('Password').click();
-});
